08d5fb5c2e50c4000032f0efa68286ddef58d41f
Bruno Spoladore <brunospoladore@hotmail.com>
2022-10-07 21:20:01 -0300

    exercise 4

--- MODULO 1/bruno_spoladore/EXERCICIO 4/modules.py
+++ MODULO 1/bruno_spoladore/EXERCICIO 4/modules.py
@@ -3,10 +3,27 @@ import math
 
 
 def frequencia(vetor):
-    n = math.sqrt(len(vetor))
+    tabela_frequencia = []
+    tamanho_vetor = len(vetor)
+    n = math.sqrt(tamanho_vetor)
     maior_numero = max(vetor)
     menor_numero = min(vetor)
     amplitude = math.ceil((maior_numero - menor_numero) / n)
+    qtd_total_elementos = 0
+    i = 0
+    print('Amplitude: %d' % amplitude)
+    print('Vetor: %s' % vetor)
+    while i < math.ceil(n):
+        qtd_elementos = 0
+        limite_inferior = menor_numero + (i * amplitude)
+        limite_superior = limite_inferior + amplitude
+        for x in vetor:
+            if limite_inferior <= x < limite_superior:
+                qtd_elementos += 1
+        qtd_total_elementos += qtd_elementos
+        i += 1
+        tabela_frequencia.append([limite_inferior, limite_superior, qtd_elementos])
+    return tabela_frequencia
 
 
 def gera_vetor(tamanho_vetor):
